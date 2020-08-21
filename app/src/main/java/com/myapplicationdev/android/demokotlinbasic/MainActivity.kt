00304cd006b8bd7ab54b0671d5379c302755511e
syafzid <18052270@myrp.edu.sg>
2020-08-21 10:13:22 +0800

    Completed with Section B Worksheet

--- app/src/main/java/com/myapplicationdev/android/demokotlinbasic/MainActivity.kt
+++ app/src/main/java/com/myapplicationdev/android/demokotlinbasic/MainActivity.kt
@@ -2,6 +2,7 @@ package com.myapplicationdev.android.demokotlinbasic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
@@ -12,9 +13,38 @@ class MainActivity : AppCompatActivity() {
 
         btnGetAge.setOnClickListener {
             val userDOB = etDOB.text.toString()
-            val year = Calendar.getInstance().get(Calendar.YEAR)
-            val age = year - userDOB.toInt()
-            tvShowAge.text = "Your age is $age"
+            if (userDOB.isNotEmpty()) {
+                val year = Calendar.getInstance().get(Calendar.YEAR)
+                val age = year - userDOB.toInt()
+
+                var output = ""
+//                if (age < 18) {
+//                    output = "Underage"
+//                } else if (age in 18..65) {
+//                    output = "Young People"
+//                } else if (age in 66..79) {
+//                    output = "Middle-aged"
+//                } else if (age in 80..99) {
+//                    output = "Elderly"
+//                } else {
+//                    output = "Long-lived elderly"
+//                }
+
+                when (age) {
+                    in 0..17 -> output = "Underage"
+                    in 18..65 -> output = "Young people"
+                    in 66..79 -> output = "Middle-aged"
+                    in 80..99 -> output = "Elderly"
+
+                    else -> {
+                        output = "Long-lived elderly"
+                    }
+                }
+
+                tvShowAge.text = "Your age is $age\n$output"
+            } else {
+                Toast.makeText(this, "Please enter your birth year", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
